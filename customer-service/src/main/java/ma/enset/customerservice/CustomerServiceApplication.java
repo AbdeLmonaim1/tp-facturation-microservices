873f6c7b691042d2089e30aa3f376eafe5d3dfc3
AbdeLmonaim1 <abdelmonaimahdoud3@gmail.com>
2025-10-21 01:02:33 +0100

    config service is completed

--- customer-service/src/main/java/ma/enset/customerservice/CustomerServiceApplication.java
+++ customer-service/src/main/java/ma/enset/customerservice/CustomerServiceApplication.java
@@ -1,13 +1,16 @@
 package ma.enset.customerservice;
 
+import ma.enset.customerservice.config.CustomerRecodConfigParams;
 import ma.enset.customerservice.entities.Customer;
 import ma.enset.customerservice.repositories.CustomerRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableConfigurationProperties(CustomerRecodConfigParams.class)
 public class CustomerServiceApplication {
 
     public static void main(String[] args) {
